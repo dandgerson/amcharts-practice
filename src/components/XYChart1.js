2b682c55aca800fbfcf907365c5293a851ae255a
Dmitry G. Anderson <dandgerson@gmail.com>
2019-06-22 12:35:46 +0300

    make chart

--- src/components/XYChart1.js
+++ src/components/XYChart1.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+import am4themes_kelly from '@amcharts/amcharts4/themes/kelly';
 
 class XYChart1 extends React.Component {
   constructor(props) {
@@ -10,25 +12,49 @@ class XYChart1 extends React.Component {
   }
   
   componentDidMount() {
+    am4core.useTheme(am4themes_animated);
+    am4core.useTheme(am4themes_kelly);
+
     const chart = am4core.create('chartdiv', am4charts.XYChart);
     chart.data = this.props.data;
 
     const categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
     categoryAxis.dataFields.category = 'Team name';
-    categoryAxis.title.text = 'Teams';
-
+    categoryAxis.title.text = 'Team names';
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 20;
+    
     const valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.title.text = "Task Quantity";
-
+    valueAxis.title.text =  'Task Quantity';
+    
+    // Create series
     const series = chart.series.push(new am4charts.ColumnSeries());
-    series.dataFields.valueY = "Issue ID";
-    series.dataFields.categoryX = "Team name";
+    series.name =  'Team names ';
+    series.dataFields.valueY =  'Issue ID';
+    series.dataFields.categoryX =  'Team name';
+    series.tooltipText = '{name}: [bold]{valueY}[/]';
+    series.stacked = true;
+    
+    const series2 = chart.series.push(new am4charts.ColumnSeries());
+    series2.name = 'Status';
+    series2.dataFields.valueY = 'Issue status';
+    series2.dataFields.categoryX = 'Team name';
+    series2.tooltipText = '{name}: [bold]{valueY}[/]';
+    series2.stacked = true;
+
+    chart.cursor = new am4charts.XYCursor();
+
+    this.chart = chart;
+  }
+
+  componentWillMount() {
+    this.chart && this.chart.dispose();
   }
   
   render() {
     return (
-      <div className="chart-container">
-        <div id="chartdiv"></div>
+      <div className= 'chart-container'>
+        <div id= 'chartdiv'></div>
       </div>
     );
   }
