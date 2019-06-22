d88a6011bafb4f1eb423a07947e417f5560bf6e5
Dmitry G. Anderson <dandgerson@gmail.com>
2019-06-23 00:34:37 +0300

    add chart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-// import data1 from './data/data1.js';
+import data1 from './data/data1.js';
 // import data2 from './data/data2.js';
 // import data3 from './data/data3.js';
 // import data4 from './data/data4.js';
-import data5 from './data/data5.js';
+// import data5 from './data/data5.js';
 
 // import XYChart1 from './components/XYChart1';
 // import XYChart2 from './components/XYChart2';
 // import XYChart3 from './components/XYChart3';
 // import XYChart4 from './components/XYChart4';
-import XYChart5 from './components/XYChart5';
+// import XYChart5 from './components/XYChart5';
+import XYChart6 from './components/XYChart6';
 
 class App extends React.Component {
   render() {
@@ -20,7 +21,8 @@ class App extends React.Component {
         {/* <XYChart2 data={data2} className="chart"/> */}
         {/* <XYChart3 data={data3} className="chart"/> */}
         {/* <XYChart4 data={data4} className="chart"/> */}
-        <XYChart5 data={data5} className="chart"/>
+        {/* <XYChart5 data={data5} className="chart"/> */}
+        <XYChart6 data={data1} className="chart"/>
       </div>
     );
   }
---
+++ src/components/XYChart6.js
@@ -0,0 +1,133 @@
+import React from 'react';
+
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+// import am4themes_dataviz from '@amcharts/amcharts4/themes/dataviz';
+
+// Using adapters to modify legend item appearance
+
+class XYChart6 extends React.Component {
+  seriesCornerRadiusAdapter(chart, raduis, item) {
+    const dataItem = item.dataItem;
+  
+    let lastSeries;
+    chart.series.each(series => {
+      if (dataItem.dataContext[series.dataFields.valueY]
+        && !series.isHidden
+        && !series.isHiding) {
+        lastSeries = series;
+      }
+    });
+    return dataItem.component === lastSeries ? 10 : raduis;
+  }
+
+  createSeries({ chart, colors, field, name, stacked = false }) {
+    const series = chart.series.push(new am4charts.ColumnSeries());
+
+    series.name = name;
+    series.dataFields.valueY = field;
+    series.dataFields.categoryX = 'Team name';
+
+    series.stacked = stacked;
+
+    series.columns.template.width = am4core.percent(50);
+    series.columns.template.tooltipText = '{name}: [bold]{valueY.value}[/]';
+    series.columns.template.fill = am4core.color(colors[name]);
+    
+    series.columns.template.column.adapter.add(
+      'cornerRadiusTopLeft',
+      this.seriesCornerRadiusAdapter.bind(null, chart), // partial function
+    );
+    series.columns.template.column.adapter.add(
+      'cornerRadiusTopRight',
+      this.seriesCornerRadiusAdapter.bind(null, chart), // partial function
+      );
+    
+    return series;
+  }
+
+  componentDidMount() {
+    am4core.useTheme(am4themes_animated);
+    // am4core.useTheme(am4themes_dataviz);
+
+    const colors = {
+      'Done': '#FFBF00',
+      'In progress': '#A5A5A5',
+      'Open': '#ED7D31',
+    };
+
+    const chart = am4core.create('chartdiv6', am4charts.XYChart);
+    chart.data = this.props.data;
+
+    const categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = 'Team name';
+    categoryAxis.renderer.grid.template.location = 0;
+
+    const valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.min = 0;
+    valueAxis.renderer.minWidth = 35;
+
+    chart.cursor = new am4charts.XYCursor();
+
+    chart.legend = new am4charts.Legend();
+
+    // Create series
+
+    this.createSeries({
+      chart,
+      colors,
+      field: 'Issue ID',
+      name: 'Done',
+      stacked: true,
+    });
+    this.createSeries({
+      chart,
+      colors,
+      field: 'Issue ID',
+      name: 'In progress',
+      stacked: true,
+    });
+    this.createSeries({
+      chart,
+      colors,
+      field: 'Issue ID',
+      name: 'Open',
+      stacked: true,
+    });
+
+    // TODO show total
+
+    const totalSeries = chart.series.push(new am4charts.ColumnSeries());
+    totalSeries.dataFields.valueY = 'none';
+    totalSeries.dataFields.categoryX = 'Team name';
+    totalSeries.stacked = true;
+    totalSeries.hiddenInLegend = true;
+    totalSeries.columns.template.strokeOpacity = 0;
+
+    const totalBullet = totalSeries.bullets.push(new am4charts.LabelBullet());
+    totalBullet.dy = 0;
+    totalBullet.label.text = '{valueY.total}';
+    totalBullet.label.hideOversized = false;
+    totalBullet.label.fontSize = 18;
+    totalBullet.label.background.fill = totalSeries.stroke;
+    totalBullet.label.background.fillOpacity = 0.2;
+    totalBullet.label.padding(5, 10, 5, 10);
+
+    this.chart = chart;
+  }
+
+  componentWillMount() {
+    this.chart && this.chart.dispose();
+  }
+  
+  render() {
+    return (
+      <div className= 'chart-container'>
+        <div id= 'chartdiv6' className="chart"></div>
+      </div>
+    );
+  }
+}
+
+export default XYChart6;
